849996df2dd93e14bafc75f6daf4a35dea13da50
termslang <m.baynov@gmail.com>
2019-01-03 14:05:08 +0300

    ocaml

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
 (delete-other-windows)
 (split-window-horizontally)
 (next-multiframe-window)
-(setf initial-buffer-choice "~/haskell/")
+(setf initial-buffer-choice "~/ocaml/")
 ;; scrolling
 (setq scroll-step 1
    scroll-conservatively 10000)
@@ -258,3 +258,6 @@
 
 
 
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
